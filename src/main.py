bfaa35158510ef6fe40f9b0dcd617e0b1898686c
synrocka <asdwklim@gmail.com>
2024-03-26 19:28:02 +0300

    Edit main.py

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ from fastapi import FastAPI, UploadFile
 # Import функций с моделями
 from transcribe import load_model, transcribe  # Функция перевода аудио в текст
 # Функция определения категории и приоритетности запроса по тексту
-from categorize import categorize_ticket
+import categorize
 
 app = FastAPI()  # Создаем приложение в переменной app
 model = load_model()  # Загружаем модель в переменную model
@@ -26,7 +26,7 @@ async def process(audio_file: UploadFile):
     """
     content = audio_file.file.read()
     transcription = transcribe(model, content)
-    categorization = categorize_ticket(transcription)
+    categorization = categorize.process_texts(transcription)
     return {"Transcription": transcription,
             "Category": categorization["category"],
             "Priority": categorization["priority"]}
