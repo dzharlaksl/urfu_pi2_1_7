d925d8f4e21bdabfd95af24eb9aafa9981548277
Евгений <bev14159@gmail.com>
2024-03-31 16:44:39 +0500

    Deleted django app, since it's not used currently. So it's a dead code and it's better to delete it.

--- src/frontend/firstapp/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Client, Category, Employee, Ticket
-
-admin.site.register(Client)
-admin.site.register(Category)
-admin.site.register(Employee)
-admin.site.register(Ticket)
